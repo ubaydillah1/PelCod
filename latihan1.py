9a254bf21e2a7b6326942cbc1c29a5e3f7d3c28b
ubaydillah1 <143859785+ubaydillah1@users.noreply.github.com>
2023-09-02 11:59:30 +0800

    Add files via upload
    Upload File

---
+++ latihan1.py
@@ -0,0 +1,74 @@
+
+
+nama_saya = str(input("masukkan nama kamu = "))
+print(nama_saya,"bertipe",type(nama_saya))
+
+umur = int(input("masukkan umur kamu = "))
+print(umur,"bertipe",type(umur))
+
+IPK = float(input("masukkan IPK kamu = "))
+print(IPK, "bertipe", type(IPK))
+
+menikah = bool(input("masukkan True or False = "))
+print(menikah, "bertipe", type(menikah))
+
+# matkul_diambil = ["alpro", Ubay"sig"]
+# print(matkul_diambil, "bertipe", type(matkul_diambil))
+
+# matkul_diambil2 = ("web", "pemvis")
+# print("Matkul Lain: ", matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+# matkul_diambil3 = {"Alpro", "Alpro", "Pemweb", "Data Science", "Data Science"}
+# print("Matkul Lain Lain: ", matkul_diambil3, "bertipe", type(matkul_diambil3))
+
+
+# print()
+# print()
+
+# # Penjumlahan
+# print("1" + "1")
+# print(1 + 1)
+
+# print()
+# print()
+
+# # Perkalian
+# print("he " * 5)
+# print(5 * 5)
+
+# print()
+# print()
+
+# # Pembagian
+
+# print(17 / 3)
+# print(17 // 3) # Dibulatkan ke bawah
+
+# print()
+# print()
+
+# # Modulus
+# print(17 % 3)
+
+# print()
+# print()
+
+# # Menggunakan Variable
+
+# nama_universitas = 'Universitas Trunojoyo'
+# print(nama_universitas)
+
+# print()
+# print()
+
+# nama_universitas = str(input("Masukkan Nama :"))
+# print(nama_universitas)
+
+
+
+
+
+
+
+
+
